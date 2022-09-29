62dc9088fecfb1039beb26eef58742353732161c
Olegario <olegario.fernandezalonso@vtsd.com>
2022-09-29 10:03:34 -0400

    code update

--- Lab04avst.java
+++ Lab04avst.java
@@ -12,6 +12,10 @@ public class Lab04avst
         double principal  = 250000;
         double annualRate = 4.85;
         double numYears   = 30;
+        double nmp = numYears*12;
+        double mr = (annualRate/12)/100;
+        double n1 = Math.pow((1 + mr),nmp);
+        double n2 = ((mr * n1)/(n1 - 1)) * principal;
 
 
 
@@ -21,7 +25,10 @@ public class Lab04avst
 
 
 
-        System.out.println();
+        System.out.println("Principal:          $" + principal);
+        System.out.println("Annual Rate:        " + annualRate +"%");
+        System.out.println("Number of Years:    " + numYears);
+        System.out.println("Monthly Payment:    $" + n2);
     }
 }
 
