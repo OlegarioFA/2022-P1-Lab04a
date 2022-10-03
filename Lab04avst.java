72f79bf53a7da19b38a36b38099e6af2e534577a
Olegario <olegario.fernandezalonso@vtsd.com>
2022-10-03 08:33:44 -0400

    code update

--- Lab04avst.java
+++ Lab04avst.java
@@ -11,19 +11,21 @@ public class Lab04avst {
         double annualRate = 4.85;
         double numYears = 30;
         double nmp = numYears * 12;
-        double mr = (annualRate / 12) / 100;
-        double n1 = Math.pow((1 + mr), nmp);
-        double n2 = (((mr * n1) / (n1 - 1)) * principal);
-        double n3 = ((n2 * 12) * 30);
-        double n4 = (n3 - principal);
+        double monthlyrate = (annualRate / 12) / 100;
+        double Calculation1 = Math.pow((1 + monthlyrate), nmp);
+        double MonthlyPayment = (((monthlyrate * Calculation1) / (Calculation1 - 1)) * principal);
+        double TotalPayment = ((MonthlyPayment * 12) * 30);
+        double TotalInterest =(TotalPayment - principal);
 
 
-        System.out.println("Principal:          $" + principal);
+
+
+        System.out.println("Principal:         $" + principal);
         System.out.println("Annual Rate:        " + annualRate + "%");
         System.out.println("Number of Years:    " + numYears);
-        System.out.println("Monthly Payment:    $" + n2);
-        System.out.println("Total Payment:      $" + n3);
-        System.out.println("Total Interest:     $" + n4);
+        System.out.println("Monthly Payment:   $" + MonthlyPayment);
+        System.out.println("Total Payment:     $" + TotalPayment);
+        System.out.println("Total Interest:    $" + TotalInterest);
     }
 }
 
